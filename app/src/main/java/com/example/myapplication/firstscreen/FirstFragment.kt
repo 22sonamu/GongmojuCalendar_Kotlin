a7d8ac890b54e3ac226ba07092f73713fde38a70
여정화 <22sonamu@naver.com>
2022-02-08 23:19:18 +0900

    first

---
+++ app/src/main/java/com/example/myapplication/firstscreen/FirstFragment.kt
@@ -0,0 +1,34 @@
+package com.example.myapplication.firstscreen
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import com.example.myapplication.R
+import kotlinx.android.synthetic.main.fragment_first.*
+
+class FirstFragment : Fragment(){
+
+    lateinit var navController: NavController
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment_first, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = Navigation.findNavController(view)
+
+        button.setOnClickListener{
+            navController.navigate(R.id.action_firstFragment_to_secondFragment)
+        }
+
+    }
+}
